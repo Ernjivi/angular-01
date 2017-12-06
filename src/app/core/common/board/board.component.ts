71de777076e9ce1793808722a81fca39aa47aa01
Ernesto Jiménez Villaseñor <ernesto@bedu.org>
2017-12-06 16:42:41 -0600

    Added board component

---
+++ src/app/core/common/board/board.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import {  } from "../";
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
+})
+export class BoardComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
