7ec5f7371fa013b56c638577eaf8736e50d20eb2
Ernesto Jiménez Villaseñor <ernesto@bedu.org>
2017-12-07 13:16:33 -0600

    Added DragulaModule to main module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { FormsModule } from "@angular/forms";
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from "@angular/router";
 
+import { DragulaModule } from "ng2-dragula";
 
 import { AppComponent } from './app.component';
 import { CheklistComponent } from './core/common/checklists/cheklist/cheklist.component';
 import { CheckComponent } from './core/common/checklists/check/check.component';
 import { CardComponent } from './core/common/card/card.component';
+import { BoardComponent } from './core/common/board/board.component';
 
 const routes: Routes = [
-  { path: '', component: CardComponent }
+  { path: '', component: BoardComponent }
 ]
 
 
@@ -19,12 +21,14 @@ const routes: Routes = [
     AppComponent,
     CheklistComponent,
     CheckComponent,
-    CardComponent
+    CardComponent,
+    BoardComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot(routes),
+    DragulaModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
